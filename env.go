bde96d641fa578a3b86107f41ead94dcda6cb9ee
fabriciojs <fajosesouza@gmail.com>
2020-05-25 20:47:00 -0300

    Adjust env; fallback to os.Getenv

--- env.go
+++ env.go
@@ -5,18 +5,20 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
-	"github.com/joho/godotenv"
+	"github.com/fireworkweb/godotenv"
 	homedir "github.com/mitchellh/go-homedir"
 )
 
 func initEnvironmentVariables() {
 	var (
-		files   []string
-		homeDir string
-		err     error
+		files            []string
+		homeDir, workDir string
+		err              error
 	)
 
+	// TODO: we need to read fwd distributed envs as well
 	files = []string{
 		".env",
 		".fwd",
@@ -25,6 +27,20 @@ func initEnvironmentVariables() {
 	homeDir, err = homedir.Dir()
 	if err != nil {
 		log.Fatal("Could not evaluate HOME directory - ", err)
+	}
+	if os.Getenv("HOME") == "" {
+		os.Setenv("HOME", homeDir)
+	}
+	if os.Getenv("UID") == "" {
+		os.Setenv("UID", fmt.Sprintf("%d", os.Getuid()))
+	}
+
+	if os.Getenv("PWD") == "" {
+		workDir, err = os.Getwd()
+		if err != nil {
+			log.Fatal("Could not evaluate working directory - ", err)
+		}
+		os.Setenv("PWD", workDir)
 	}
 
 	files = append(files, fmt.Sprintf("%s%s.fwd", homeDir, string(filepath.Separator)))
@@ -38,5 +54,19 @@ func initEnvironmentVariables() {
 		if err != nil {
 			log.Fatal("Failure loading environment file ", file, " ERROR: '", err, "'")
 		}
+	}
+
+	// Now that we loaded up the files, we will check for
+	// missing variables that we need to fix
+	if os.Getenv("FWD_NAME") == "" {
+		pieces := strings.Split(os.Getenv("PWD"), string(os.PathSeparator))
+		os.Setenv("FWD_NAME", pieces[len(pieces)-1])
+	}
+
+	if os.Getenv("FWD_NETWORK") == "" {
+		os.Setenv("FWD_NETWORK", "fwd_global")
+	}
+	if os.Getenv("FWD_ASUSER") == "" {
+		os.Setenv("FWD_ASUSER", fmt.Sprintf("%d", os.Getuid()))
 	}
 }
