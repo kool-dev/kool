426ff7b4450f2e86b10fbb1ecc03fde309dd16dc
fabriciojs <fajosesouza@gmail.com>
2020-05-25 20:47:47 -0300

    Minor tweaks

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,18 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "fwd",
-	Short: "fwd - FWD Web Development - easy environments",
+	Short: "fwd - fwd - Fucking aWesome Development",
 	Long: `An easy and robust software development environment
 					tool helping you from project creation until deployment.
 					Complete documentation is available at https://fwd.tools`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("fwd - Fucking aWesome Development")
-	},
 }
 
+// Execute proxies the call to cobra root command
 func Execute() error {
 	return rootCmd.Execute()
 }
