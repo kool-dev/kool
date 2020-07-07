dc83bf2915f58715c02a66e896fad87143210434
fabriciojs <fajosesouza@gmail.com>
2020-07-07 17:39:09 -0300

    add arm6/7 artifacts generation for release attaching

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -9,3 +9,11 @@ docker run --rm --env GOOS=darwin --env GOARCH=amd64 --env CGO_ENABLED=0 -v $(pw
 echo "Building to GOOS=linux GOARCH=amd64"
 
 docker run --rm --env GOOS=linux --env GOARCH=amd64 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-amd64
+
+echo "Building to GOOS=linux GOARCH=arm GOARM=6"
+
+docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=6 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm6
+
+echo "Building to GOOS=linux GOARCH=arm GOARM=7"
+
+docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=7 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm7
