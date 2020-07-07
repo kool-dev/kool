cb238a06fcde93e931ad98eaf631be5f1e2ab910
fabriciojs <fajosesouza@gmail.com>
2020-07-07 18:03:10 -0300

    added checksum

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -17,3 +17,9 @@ docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=6 --env CGO_ENABLE
 echo "Building to GOOS=linux GOARCH=arm GOARM=7"
 
 docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=7 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm7
+
+echo "Going to generate CHECKSUMS"
+
+for file in dist/*; do
+	shasum $file | awk '{print $1}' > $file.checksum
+done
