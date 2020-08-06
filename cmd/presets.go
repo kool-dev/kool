24c917124738d239bc7cdc5549751b40e600fd6e
Daniel Polito <danielbpolito@gmail.com>
2020-08-06 13:57:58 -0300

    Improving Presets

--- cmd/presets.go
+++ cmd/presets.go
@@ -4,139 +4,6 @@ package cmd
 
 var presets = make(map[string]map[string]string)
 func init() {
-	presets["adonis-nextjs"] = map[string]string{
-		"Dockerfile.adonis.build": `FROM kooldev/node:14-adonis AS build
-COPY . /app
-
-RUN npm install
-
-FROM kooldev/node:14-adonis
-
-COPY --from=build --chown=kool:kool /app /app
-
-EXPOSE 3333
-
-CMD [ "npm", "adonis:start" ]`,
-		"Dockerfile.nextjs.build": `FROM kooldev/node:14 AS build
-
-COPY . /app
-
-RUN npm install && npm run build
-
-FROM kooldev/node:14
-
-COPY --from=build --chown=kool:kool /app /app
-
-EXPOSE 3000
-
-CMD [ "npm", "nextjs:start" ]`,
-		"docker-compose.yml": `version: "3.8"
-services:
-  adonis:
-    image: kooldev/node:14-adonis
-    command: ["adonis", "serve", "--dev"]
-    ports:
-     - "${PORT:-3333}:3333"
-    environment:
-      ASUSER: "${KOOL_ASUSER:-0}"
-      UID: "${UID:-0}"
-    volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
-    networks:
-     - kool_local
-     - kool_global
-  nextjs:
-    image: kooldev/node:14
-    command: ["npm", "run", "nextjs:dev"]
-    ports:
-     - "${KOOL_NEXTJS_PORT:-3000}:3000"
-    environment:
-      ASUSER: "${KOOL_ASUSER:-0}"
-      UID: "${UID:-0}"
-    volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
-    networks:
-     - kool_local
-     - kool_global
-
-##########################################################################
-# optionally you can enable mysql, redis or other services to your stack #
-##########################################################################
-#   database:
-#     image: mysql:8.0 # possibly change to: mysql:5.7
-#     ports:
-#      - "3306:3306"
-#     environment:
-#       MYSQL_ROOT_PASSWORD: "${DB_PASSWORD:-rootpass}"
-#       MYSQL_DATABASE: "${DB_DATABASE:-database}"
-#       MYSQL_USER: "${DB_USERNAME:-user}"
-#       MYSQL_PASSWORD: "${DB_PASSWORD:-}"
-#       MYSQL_ALLOW_EMPTY_PASSWORD: "yes"
-#     volumes:
-#      - db:/var/lib/mysql:cached
-#     networks:
-#      - kool_local
-#   cache:
-#     image: redis:6-alpine
-#     volumes:
-#      - cache:/data:cached
-#     networks:
-#      - kool_local
-#
-# volumes:
-#   db:
-#   cache:
-
-networks:
-  kool_local:
-  kool_global:
-    external: true
-    name: "${KOOL_GLOBAL_NETWORK:-kool_global}"`,
-		"kool.env": `HOST=0.0.0.0
-PORT=3333
-NODE_ENV=development
-APP_URL=http://localhost:${PORT}`,
-		"kool.package.json": `{
-  "name": "adonis-nextjs",
-  "private": true,
-  "description": "AdonisJS and NextJS",
-  "scripts": {
-    "adonis:start": "node server.js",
-    "adonis:test": "node ace test",
-    "nextjs:dev": "next",
-    "nextjs:build": "next build",
-    "nextjs:start": "next start"
-  },
-  "dependencies": {
-    "@adonisjs/ace": "^5.0.8",
-    "@adonisjs/auth": "^3.0.7",
-    "@adonisjs/bodyparser": "^2.0.5",
-    "@adonisjs/cors": "^1.0.7",
-    "@adonisjs/fold": "^4.0.9",
-    "@adonisjs/framework": "^5.0.9",
-    "@adonisjs/ignitor": "^2.0.8",
-    "@adonisjs/lucid": "^6.1.3",
-    "next": "^9.4.4",
-    "react": "^16.13.1",
-    "react-dom": "^16.13.1"
-  },
-  "devDependencies": {},
-  "autoload": {
-    "App": "./app"
-  }
-}`,
-		"kool.yml": `scripts:
-  node: kool exec app node
-  npm: kool exec app npm # can change to: yarn,pnpm
-  adonis: kool exec adonis adonis
-
-  setup:
-    - cp .env.example .env
-    - kool docker kooldev/node:14 npm install # can change to: yarn,pnpm
-    - kool start`,
-	}
 	presets["adonis"] = map[string]string{
 		"Dockerfile.build": `FROM kooldev/node:14-adonis AS build
 
@@ -151,19 +18,19 @@ COPY --from=build --chown=kool:kool /app /app
 EXPOSE 3333
 
 CMD [ "npm", "start" ]`,
-		"docker-compose.yml": `version: "3.8"
+		"docker-compose.yml": `version: "3.7"
 services:
   app:
     image: kooldev/node:14-adonis
     command: ["adonis", "serve", "--dev"]
     ports:
-     - "${PORT:-3333}:3333"
+     - "${KOOL_APP_PORT:-3333}:3333"
     environment:
       ASUSER: "${KOOL_ASUSER:-0}"
       UID: "${UID:-0}"
     volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
     networks:
      - kool_local
      - kool_global
@@ -178,13 +45,13 @@ services:
 #       MYSQL_PASSWORD: "${DB_PASSWORD:-}"
 #       MYSQL_ALLOW_EMPTY_PASSWORD: "yes"
 #     volumes:
-#      - db:/var/lib/mysql:cached
+#      - db:/var/lib/mysql:delegated
 #     networks:
 #      - kool_local
 #   cache:
 #     image: redis:6-alpine
 #     volumes:
-#      - cache:/data:cached
+#      - cache:/data:delegated
 #
 # volumes:
 #   db:
@@ -223,18 +90,18 @@ RUN yarn install && yarn prod
 FROM kooldev/php:7.4-nginx
 
 COPY --from=node --chown=kool:kool /app /app`,
-		"docker-compose.yml": `version: "3.8"
+		"docker-compose.yml": `version: "3.7"
 services:
   app:
     image: kooldev/php:7.4-nginx
     ports:
-     - "${KOOL_HTTP_PORT:-80}:80"
+     - "${KOOL_APP_PORT:-80}:80"
     environment:
       ASUSER: "${KOOL_ASUSER:-0}"
       UID: "${UID:-0}"
     volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
     networks:
      - kool_local
      - kool_global
@@ -250,13 +117,13 @@ services:
       MYSQL_PASSWORD: "${DB_PASSWORD:-}"
       MYSQL_ALLOW_EMPTY_PASSWORD: "yes"
     volumes:
-     - db:/var/lib/mysql:cached
+     - db:/var/lib/mysql:delegated
     networks:
      - kool_local
   cache:
     image: redis:6-alpine
     volumes:
-     - cache:/data:cached
+     - cache:/data:delegated
     networks:
      - kool_local
 
@@ -276,6 +143,9 @@ networks:
   node: kool docker kooldev/node:14 node
   npm: kool docker kooldev/node:14 npm # can change to: yarn,pnpm
 
+  mysql: kool exec database mysql -uroot -p$DB_PASSWORD
+  mysql-no-tty: kool exec --disable-tty database mysql -uroot -p$DB_PASSWORD
+
   setup:
     - kool start
     - cp .env.example .env
@@ -290,6 +160,92 @@ networks:
     - kool run npm install
     - kool run npm run dev`,
 	}
+	presets["nestjs"] = map[string]string{
+		"Dockerfile.build": `FROM kooldev/node:14-nest AS build
+
+COPY . /app
+
+RUN npm install
+
+FROM kooldev/node:14-nest
+
+COPY --from=build --chown=kool:kool /app /app
+
+EXPOSE 3000
+
+CMD [ "npm", "start" ]`,
+		"docker-compose.yml": `version: "3.7"
+services:
+  app:
+    image: kooldev/node:14-nest
+    command: ["npm", "run", "start:dev"]
+    ports:
+     - "${KOOL_APP_PORT:-3000}:3000"
+    environment:
+      ASUSER: "${KOOL_ASUSER:-0}"
+      UID: "${UID:-0}"
+    volumes:
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
+    networks:
+     - kool_local
+     - kool_global
+#   database:
+#     image: mysql:8.0
+#     ports:
+#      - "${KOOL_DATABASE_PORT:-3306}:3306"
+#     environment:
+#       MYSQL_ROOT_PASSWORD: "${DB_PASSWORD:-rootpass}"
+#       MYSQL_DATABASE: "${DB_DATABASE:-database}"
+#       MYSQL_USER: "${DB_USERNAME:-user}"
+#       MYSQL_PASSWORD: "${DB_PASSWORD:-}"
+#       MYSQL_ALLOW_EMPTY_PASSWORD: "yes"
+#     volumes:
+#      - db:/var/lib/mysql:delegated
+#     networks:
+#      - kool_local
+#   cache:
+#     image: redis:6-alpine
+#     volumes:
+#      - cache:/data:delegated
+#   mongo:
+#     image: mongo:4
+#     ports:
+#      - "${KOOL_MONGO_PORT:-27017}:27017"
+#     environment:
+#       MONGO_INITDB_ROOT_USERNAME: "${MONGO_USERNAME:-root}"
+#       MONGO_INITDB_ROOT_PASSWORD: "${MONGO_PASSWORD:-rootpass}"
+#       MONGO_INITDB_DATABASE: "${MONGO_DATABASE:-database}"
+#     volumes:
+#      - mongo:/data/db:delegated
+#     networks:
+#      - kool_local
+#
+# volumes:
+#   db:
+#   cache:
+#   mongo:
+
+networks:
+  kool_local:
+  kool_global:
+    external: true
+    name: "${KOOL_GLOBAL_NETWORK:-kool_global}"`,
+		"kool.yml": `scripts:
+  node: kool exec app node
+  npm: kool exec app npm # can change to: yarn,pnpm
+  nest: kool exec app nest
+
+  mysql: kool exec database mysql -uroot -prootpass
+  mysql-no-tty: kool exec --disable-tty database mysql -uroot -prootpass
+
+  mongo: kool exec mongo mongo -uroot -prootpass
+  mongo-no-tty: kool exec --disable-tty mongo mongo -uroot -prootpass
+
+  setup:
+    - kool docker kooldev/node:14 npm install # can change to: yarn,pnpm
+    - kool start`,
+	}
 	presets["nextjs-static"] = map[string]string{
 		"Dockerfile.build": `FROM kooldev/node:14 AS node
 
@@ -302,7 +258,7 @@ FROM kooldev/http:static
 ENV ROOT=/app/out
 
 COPY --from=node /app/out /app/out`,
-		"docker-compose.yml": `version: "3.8"
+		"docker-compose.yml": `version: "3.7"
 services:
   app:
     image: kooldev/node:14
@@ -313,8 +269,8 @@ services:
       ASUSER: "${KOOL_ASUSER:-0}"
       UID: "${UID:-0}"
     volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
     networks:
      - kool_local
      - kool_global
@@ -346,7 +302,7 @@ COPY --from=build --chown=kool:kool /app /app
 EXPOSE 3000
 
 CMD [ "npm", "start" ]`,
-		"docker-compose.yml": `version: "3.8"
+		"docker-compose.yml": `version: "3.7"
 services:
   app:
     image: kooldev/node:14
@@ -357,8 +313,8 @@ services:
       ASUSER: "${KOOL_ASUSER:-0}"
       UID: "${UID:-0}"
     volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
     networks:
      - kool_local
      - kool_global
@@ -388,7 +344,7 @@ FROM kooldev/http:static
 ENV ROOT=/app/dist
 
 COPY --from=node /app/dist /app/dist`,
-		"docker-compose.yml": `version: "3.8"
+		"docker-compose.yml": `version: "3.7"
 services:
   app:
     image: kooldev/node:14
@@ -399,8 +355,8 @@ services:
       ASUSER: "${KOOL_ASUSER:-0}"
       UID: "${UID:-0}"
     volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
     networks:
      - kool_local
      - kool_global
@@ -437,7 +393,7 @@ COPY --from=build --chown=kool:kool /app /app
 EXPOSE 3000
 
 CMD [ "npm", "start" ]`,
-		"docker-compose.yml": `version: "3.8"
+		"docker-compose.yml": `version: "3.7"
 services:
   app:
     image: kooldev/node:14
@@ -448,8 +404,8 @@ services:
       ASUSER: "${KOOL_ASUSER:-0}"
       UID: "${UID:-0}"
     volumes:
-     - .:/app:cached
-    #  - $HOME/.ssh:/home/kool/.ssh:cached
+     - .:/app:delegated
+    #  - $HOME/.ssh:/home/kool/.ssh:delegated
     networks:
      - kool_local
      - kool_global
