5a971cef250f6aae9456ad41ede80ca4387a718a
fabriciojs <fajosesouza@gmail.com>
2020-08-17 11:25:46 -0300

    updates to adopt kool using Go 1.15

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bash parse_presets.sh
-
-go build -o /usr/local/bin/kool
-
-# docker run --rm --env GOOS=linux --env GOARCH=amd64 -v $(pwd):/code -w /code golang:1.14 go build -tags netgo -ldflags '-extldflags "-static"' -o dist/kool-linux-amd64
--- build_artifacts.sh
+++ build_artifacts.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+    source .env
+fi
+
+GO_IMAGE=${GO_IMAGE:-golang:1.15.0}
+
 rm -rf dist
 mkdir -p dist
 
 echo "Building to GOOS=darwin GOARCH=amd64"
 
-docker run --rm --env GOOS=darwin --env GOARCH=amd64 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-darwin-x86_64
+docker run --rm --env GOOS=darwin --env GOARCH=amd64 --env CGO_ENABLED=0 -v $(pwd):/code -w /code $GO_IMAGE go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-darwin-x86_64
 
 echo "Building to GOOS=linux GOARCH=amd64"
 
-docker run --rm --env GOOS=linux --env GOARCH=amd64 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-x86_64
+docker run --rm --env GOOS=linux --env GOARCH=amd64 --env CGO_ENABLED=0 -v $(pwd):/code -w /code $GO_IMAGE go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-x86_64
 
 echo "Building to GOOS=linux GOARCH=arm GOARM=6"
 
-docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=6 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm6
+docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=6 --env CGO_ENABLED=0 -v $(pwd):/code -w /code $GO_IMAGE go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm6
 
 echo "Building to GOOS=linux GOARCH=arm GOARM=7"
 
-docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=7 --env CGO_ENABLED=0 -v $(pwd):/code -w /code golang:1.14 go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm7
+docker run --rm --env GOOS=linux --env GOARCH=arm --env GOARM=7 --env CGO_ENABLED=0 -v $(pwd):/code -w /code $GO_IMAGE go build -a -tags 'osusergo netgo static_build' -ldflags '-extldflags "-static"' -o dist/kool-linux-arm7
 
 echo "Going to generate CHECKSUMS"
 
