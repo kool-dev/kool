2958dab6a19f353e10e3f4a3c1991bc46ab7fc39
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-09-09 10:42:42 -0300

    feat(envs): Env variables on docker and exec

--- cmd/docker.go
+++ cmd/docker.go
@@ -10,13 +10,13 @@ import (
 // DockerFlags holds the flags for the docker command
 type DockerFlags struct {
 	DisableTty bool
+	EnvVariables []string
 }
 
 var dockerCmd = &cobra.Command{
 	Use:                "docker [options] [image] [command]",
 	Args:               cobra.MinimumNArgs(1),
 	Run:                runDocker,
-	DisableFlagParsing: true,
 	Short:              "Creates a new container and runs the command in it.",
 	Long: `This command acts as a helper for docker run.
 You can start with options that go before the image name
@@ -24,35 +24,30 @@ for docker run itself, i.e --env='VAR=VALUE'. Then you must pass
 the image name and the command you want to exucute on that image.`,
 }
 
-var dockerFlags = &DockerFlags{false}
+var dockerFlags = &DockerFlags{false, []string{}}
 
 func init() {
 	rootCmd.AddCommand(dockerCmd)
 
 	dockerCmd.Flags().BoolVarP(&dockerFlags.DisableTty, "disable-tty", "T", false, "Disables TTY")
+	dockerCmd.Flags().StringArrayVarP(&dockerFlags.EnvVariables, "env", "e", []string{}, "Environment variables")
+
+	//After a non-flag arg, stop parsing flags
+	dockerCmd.Flags().SetInterspersed(false)
 }
 
-func runDocker(cmd *cobra.Command, originalArgs []string) {
-	var (
-		args []string
-	)
+func runDocker(docker *cobra.Command, args []string) {
+	image := args[0]
+	command := args[1:]
 
-	if originalArgs[0] == "--disable-tty" || originalArgs[0] == "-T" {
-		dockerFlags.DisableTty = true
-		args = originalArgs[1:]
-	} else {
-		args = originalArgs
-	}
-
-	execDockerRun(args)
+	execDockerRun(image, command)
 }
 
-func execDockerRun(command []string) {
+func execDockerRun(image string, command []string) {
 	var (
 		args    []string
 		err     error
 		workDir string
-		image   string
 	)
 
 	workDir, err = os.Getwd()
@@ -61,25 +56,16 @@ func execDockerRun(command []string) {
 		args = append(args, "-ti")
 	}
 
-	for i, arg := range command {
-		if !strings.HasPrefix(arg, "-") {
-			// we found the image!
-			image = arg
-
-			// this means the expected command is only the remaining items
-			// so we override it
-			command = command[i+1:]
-			break
-		}
-
-		// didn't find the image (first non-option argument)
-		// so we assume it is a pre-image option for docker runs
-		args = append(args, arg)
-	}
-
 	if asuser := os.Getenv("KOOL_ASUSER"); asuser != "" && (strings.HasPrefix(image, "kooldev") || strings.HasPrefix(image, "fireworkweb")) {
 		args = append(args, "--env", "ASUSER="+os.Getenv("KOOL_ASUSER"))
 	}
+
+	if len(dockerFlags.EnvVariables) > 0 {
+		for _, envVar := range dockerFlags.EnvVariables {
+			args = append(args, "--env", envVar)
+		}
+	}
+
 	args = append(args, "--volume", workDir+":/app", image)
 	args = append(args, command...)
 
