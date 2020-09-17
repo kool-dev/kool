768cefc2c497a4b965e84952ba5c66485169106c
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-09-17 17:17:07 -0300

    tests(info): Adding tests to info command

---
+++ cmd/info_test.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+	"bytes"
+	"io/ioutil"
+	"testing"
+	"strings"
+	"github.com/fireworkweb/godotenv"
+)
+
+const testingEnv string = `
+KOOL_TESTING=1
+KOOL_FILTER_TESTING=1
+`
+
+func setup() {
+	testingEnv, _ := godotenv.Unmarshal(testingEnv)
+	for k, v := range testingEnv {
+		os.Setenv(k, v)
+	}
+}
+
+func TestInfo(t *testing.T) {
+	setup()
+
+	output, err := execCommand(NewInfoCmd())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := strings.Trim(testingEnv, "\n")
+
+	if output != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, output)
+	}
+}
+
+func TestFilteredInfo(t *testing.T) {
+	setup()
+
+	cmd := NewInfoCmd()
+	cmd.SetArgs([]string{"FILTER"})
+
+	output, err := execCommand(cmd)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "KOOL_FILTER_TESTING=1"
+
+	if output != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, output)
+	}
+}
+
+func execCommand(cmd *cobra.Command) (output string, err error) {
+	b := bytes.NewBufferString("")
+	cmd.SetOut(b)
+	cmd.Execute()
+
+	out, err := ioutil.ReadAll(b)
+
+	if err != nil {
+		return
+	}
+
+	output = strings.Trim(string(out), "\n")
+	return
+}
