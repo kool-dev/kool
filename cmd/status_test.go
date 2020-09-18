f85d1132411cb7828022826716a644113b5f3138
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-09-17 21:15:52 -0300

    tests(status): Refactoring status command for testing

---
+++ cmd/status_test.go
@@ -0,0 +1,137 @@
+package cmd
+
+import (
+	"bytes"
+	"github.com/gookit/color"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+type FakeStatusCmd struct{}
+
+// CheckDependencies check kool dependencies
+func (s *FakeStatusCmd) CheckDependencies() (err error) {
+	return
+}
+
+// GetServices get docker services
+func (s *FakeStatusCmd) GetServices() (services []string, err error) {
+	services = []string{"app", "database", "cache"}
+	return
+}
+
+// GetServiceID get docker service ID
+func (s *FakeStatusCmd) GetServiceID(service string) (serviceID string, err error) {
+	serviceID = "100"
+	return
+}
+
+// GetStatusPort get docker service port and status
+func (s *FakeStatusCmd) GetStatusPort(serviceID string) (status string, port string) {
+	status = "Up About an hour"
+	port = "0.0.0.0:80->80/tcp, 9000/tcp"
+	return
+}
+
+type NotRunningStatusCmd struct {
+	FakeStatusCmd
+}
+
+// GetStatusPort get docker service port and status
+func (s *NotRunningStatusCmd) GetStatusPort(serviceID string) (status string, port string) {
+	return
+}
+
+type NoServicesStatusCmd struct {
+	FakeStatusCmd
+}
+
+// GetServices get docker services
+func (s *NoServicesStatusCmd) GetServices() (services []string, err error) {
+	return
+}
+
+func TestStatusCommand(t *testing.T) {
+	cmd := NewStatusCommand(&FakeStatusCmd{})
+	output, err := execCommand(cmd)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `
++----------+---------+------------------------------+------------------+
+| SERVICE  | RUNNING | PORTS                        | STATE            |
++----------+---------+------------------------------+------------------+
+| app      | Running | 0.0.0.0:80->80/tcp, 9000/tcp | Up About an hour |
+| cache    | Running | 0.0.0.0:80->80/tcp, 9000/tcp | Up About an hour |
+| database | Running | 0.0.0.0:80->80/tcp, 9000/tcp | Up About an hour |
++----------+---------+------------------------------+------------------+
+`
+	expected = strings.Trim(expected, "\n")
+	output = strings.Trim(output, "\n")
+
+	if output != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, output)
+	}
+}
+
+func TestNotRunningStatusCommand(t *testing.T) {
+	cmd := NewStatusCommand(&NotRunningStatusCmd{})
+	output, err := execCommand(cmd)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `
++----------+-------------+-------+-------+
+| SERVICE  | RUNNING     | PORTS | STATE |
++----------+-------------+-------+-------+
+| app      | Not running |       |       |
+| cache    | Not running |       |       |
+| database | Not running |       |       |
++----------+-------------+-------+-------+
+`
+	expected = strings.Trim(expected, "\n")
+	output = strings.Trim(output, "\n")
+
+	if output != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, output)
+	}
+}
+
+func TestNoServicesStatusCommand(t *testing.T) {
+	cmd := NewStatusCommand(&NoServicesStatusCmd{})
+	output, err := execCommand(cmd)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := color.New(color.Yellow).Sprint("No services found.")
+	output = strings.Trim(output, "\n")
+
+	if output != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, output)
+	}
+}
+
+func execCommand(cmd *cobra.Command) (output string, err error) {
+	b := bytes.NewBufferString("")
+	cmd.SetOut(b)
+
+	if err = cmd.Execute(); err != nil {
+		return
+	}
+
+	var out []byte
+	if out, err = ioutil.ReadAll(b); err != nil {
+		return
+	}
+
+	output = string(out)
+	return
+}
