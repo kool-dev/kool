8aa25611d162ce34ce915b238679d7bfeae1982a
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-09-22 14:17:46 -0300

    tests(command): added new tests to command builder

--- cmd/builder/command_test.go
+++ cmd/builder/command_test.go
@@ -1,7 +1,10 @@
 package builder
 
 import (
+	"bytes"
+	"io"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -56,5 +59,93 @@ func TestParseCommandWithUndefinedEnvironmentVariable(t *testing.T) {
 
 	if len(cmd.args) != 2 || cmd.args[1] != "VAR=" {
 		t.Errorf("ParseCommand failed to parse unset environment variable; given %s got %v", line, cmd.String())
+	}
+}
+
+func TestAppendArgs(t *testing.T) {
+	cmd := NewCommand("echo", "x1")
+
+	cmd.AppendArgs("x2")
+
+	if len(cmd.args) != 2 || cmd.args[1] != "x2" {
+		t.Errorf("AppendArgs failed to add a new argument.")
+	}
+}
+
+func TestString(t *testing.T) {
+	cmd := NewCommand("echo", "x1", "x2")
+
+	cmdString := cmd.String()
+	expected := "echo x1 x2"
+
+	if cmdString != expected {
+		t.Errorf("Failed to convert command to string; expected '%s', got '%s'", expected, cmdString)
+	}
+}
+
+func TestLookPath(t *testing.T) {
+	cmd := NewCommand("go", "version")
+
+	if err := cmd.LookPath(); err != nil {
+		t.Errorf("LookPath failed; expected no errors, got '%v'", err)
+	}
+}
+
+func TestInvalidLookPath(t *testing.T) {
+	cmd := NewCommand("fakeCommand", "version")
+
+	if err := cmd.LookPath(); err == nil {
+		t.Error("LookPath failed; expected an error, got none.")
+	}
+}
+
+func TestExec(t *testing.T) {
+	cmd := NewCommand("echo", "x")
+
+	output, err := cmd.Exec()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if output != "x" {
+		t.Errorf("Exec failed; expected output 'x', got '%s'", output)
+	}
+}
+
+func TestInteractive(t *testing.T) {
+	r, w, err := os.Pipe()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	originalOutput := os.Stdout
+	os.Stdout = w
+
+	defer func(originalOutput *os.File) {
+		os.Stdout = originalOutput
+	}(originalOutput)
+
+	cmd := NewCommand("echo", "x")
+	err = cmd.Interactive()
+
+	w.Close()
+
+	if err != nil {
+		t.Errorf("Interactive failed; expected no errors 'x', got '%v'", err)
+	}
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	output := strings.Trim(buf.String(), "\n")
+
+	if output != "x" {
+		t.Errorf("Exec failed; expected output 'x', got '%s'", output)
 	}
 }
