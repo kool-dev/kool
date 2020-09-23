388ee3a715ce322e3f9db3450a5cc685c017ed70
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-09-23 08:17:43 -0300

    fix(exit): removing hardcoded exit code

--- cmd/shell/exit.go
+++ cmd/shell/exit.go
@@ -2,16 +2,21 @@ package shell
 
 import "os"
 
+// DefaultExiter holds the default Exit behaviour
 type DefaultExiter struct{}
 
+// Exiter interface allows for interchageable usage of implementations
+// mainly for testing and extension purposes.
 type Exiter interface {
 	Exit(int)
 }
 
+// NewExiter creates a new DefaultExiter
 func NewExiter() Exiter {
 	return &DefaultExiter{}
 }
 
+// Exit implements the default Exit behaviour (proxy to OS)
 func (e *DefaultExiter) Exit(code int) {
-	os.Exit(1)
+	os.Exit(code)
 }
