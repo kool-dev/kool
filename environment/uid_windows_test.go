b19cc4413c56dccac3c159dd836ac825c28456b7
Filipe Bojikian Rissi <f.b.rissi@gmail.com>
2020-10-23 10:16:12 -0300

    Error test

--- environment/uid_windows_test.go
+++ environment/uid_windows_test.go
@@ -8,12 +8,12 @@ import (
 func TestUid(t *testing.T) {
 	current, _ := user.Current()
 
-	if "0" != uid() {
+	if "0" == uid() {
 		t.Errorf("expecting $UID value '%s', got '%s'", uid(), "0")
 	}
 
 	current.Uid = ""
-	if "0" == uid() {
+	if "0" != uid() {
 		t.Errorf("expecting $UID value '%s', got '%s'", "0", uid())
 	}
 }
