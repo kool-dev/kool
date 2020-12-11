5b47df0fa11ac7f08f9554766e15a4f66f9a3753
Nikolas Abiusi <nikolasab@gmail.com>
2020-12-11 15:42:42 -0300

    added test case [checkforupdates] for when not in terminal

--- cmd/kool_upgrade_available_checker_test.go
+++ cmd/kool_upgrade_available_checker_test.go
@@ -19,6 +19,7 @@ func newFakeUpdateAwareService(start *KoolStart, koolFakeUpdater *updater.FakeUp
 		koolFakeUpdater,
 	}
 }
+
 func TestStartWithUpdaterWrapper(t *testing.T) {
 	koolStart := &KoolStart{
 		*newFakeKoolService(),
@@ -51,7 +52,7 @@ func TestStartWithUpdaterWrapper(t *testing.T) {
 		t.Errorf("did not call CheckForUpdates")
 	}
 
-	expected := "Theres a new Kool Version available! Run kool self-update to update!"
+	expected := "There's a new version available! Run kool self-update to upgrade!"
 
 	if output := fmt.Sprint(koolStart.shell.(*shell.FakeShell).WarningOutput...); output != expected {
 		t.Errorf("expecting warning message '%s', got '%s'", expected, output)
@@ -125,6 +126,40 @@ func TestStartWithUpdaterWrapperSameVersion(t *testing.T) {
 
 	if !fakeUpdateAwareService.updater.(*updater.FakeUpdater).CalledCheckForUpdates {
 		t.Errorf("did not call CheckForUpdates")
+	}
+
+	if koolStart.exiter.(*shell.FakeExiter).Code() != 0 {
+		t.Errorf("did not expect KoolStart service to have exit code different than 0; got '%d", koolStart.exiter.(*shell.FakeExiter).Code())
+	}
+}
+
+func TestDontCheckForUpdatesWhenNonTerminal(t *testing.T)  {
+	koolStart := &KoolStart{
+		*newFakeKoolService(),
+		&checker.FakeChecker{},
+		&network.FakeHandler{},
+		environment.NewFakeEnvStorage(),
+		&builder.FakeCommand{MockCmd: "start"},
+	}
+
+	koolUpdater := &updater.FakeUpdater{
+		MockCurrentVersion: "0.0.0",
+		MockLatestVersion:  "1.0.0",
+		MockError:          nil,
+	}
+	koolStart.term.(*shell.FakeTerminalChecker).MockIsTerminal = false
+
+	cmd := NewStartCommand(koolStart)
+	fakeUpdateAwareService := newFakeUpdateAwareService(koolStart, koolUpdater)
+
+	cmd.Run = DefaultCommandRunFunction(fakeUpdateAwareService)
+
+	if fakeUpdateAwareService.updater.(*updater.FakeUpdater).CalledGetCurrentVersion {
+		t.Errorf("called GetCurrentVersion")
+	}
+
+	if fakeUpdateAwareService.updater.(*updater.FakeUpdater).CalledCheckForUpdates {
+		t.Errorf("called CheckForUpdates")
 	}
 
 	if koolStart.exiter.(*shell.FakeExiter).Code() != 0 {
