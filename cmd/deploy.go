40b0b5a0639e1aeed76767ca4ea36c1cf2276dde
fabriciojs <fajosesouza@gmail.com>
2021-01-06 11:49:04 -0300

    refactor deploy - kool service + builder commands; handling kool.deploy.env

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -3,11 +3,10 @@ package cmd
 import (
 	"fmt"
 	"kool-dev/kool/api"
-	"kool-dev/kool/cmd/shell"
+	"kool-dev/kool/cmd/builder"
 	"kool-dev/kool/environment"
 	"kool-dev/kool/tgz"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 	"time"
@@ -15,75 +14,104 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var deployCmd = &cobra.Command{
-	Use:   "deploy",
-	Short: "Deploys your application using Kool Dev",
-	Run:   runDeploy,
+const (
+	koolDeployEnv = "kool.deploy.env"
+)
+
+// KoolDeploy holds handlers and functions for using Deploy API
+type KoolDeploy struct {
+	DefaultKoolService
+
+	envStorage environment.EnvStorage
+	git        builder.Command
+}
+
+// NewDeployCommand initializes new kool deploy Cobra command
+func NewDeployCommand(deploy *KoolDeploy) *cobra.Command {
+	return &cobra.Command{
+		Use:   "deploy",
+		Short: "Deploys your application using Kool Dev",
+		Run:   DefaultCommandRunFunction(deploy),
+	}
+}
+
+// NewKoolDeploy creates a new pointer with default KoolDeploy service
+// dependencies.
+func NewKoolDeploy() *KoolDeploy {
+	defaultKoolService := newDefaultKoolService()
+	return &KoolDeploy{
+		*defaultKoolService,
+		environment.NewEnvStorage(),
+
+		builder.NewCommand("git"),
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(deployCmd)
+	rootCmd.AddCommand(NewDeployCommand(NewKoolDeploy()))
 }
 
-func runDeploy(cmd *cobra.Command, args []string) {
+// Execute runs the deploy logic.
+func (d *KoolDeploy) Execute(args []string) (err error) {
 	var (
 		filename string
 		deploy   *api.Deploy
-		err      error
 	)
 
-	if url := environment.NewEnvStorage().Get("KOOL_API_URL"); url != "" {
+	if url := d.envStorage.Get("KOOL_API_URL"); url != "" {
 		api.SetBaseURL(url)
 	}
 
-	fmt.Println("Create release file...")
-	filename, err = createReleaseFile()
+	d.Println("Create release file...")
+	filename, err = d.createReleaseFile()
 
 	if err != nil {
-		shell.Error(err)
-		os.Exit(1)
+		return
 	}
 
 	defer func(file string) {
 		var err error
 		if err = os.Remove(file); err != nil {
-			shell.Error(fmt.Errorf("error trying to remove temporary tarball: %v", err))
+			d.Error(fmt.Errorf("error trying to remove temporary tarball: %v", err))
 		}
 	}(filename)
 
 	deploy = api.NewDeploy(filename)
 
-	fmt.Println("Upload release file...")
+	d.Println("Upload release file...")
 	err = deploy.SendFile()
 
 	if err != nil {
-		shell.Error(err)
-		os.Exit(1)
+		return
 	}
 
-	fmt.Println("Going to deploy...")
+	d.Println("Going to deploy...")
 
 	timeout := 10 * time.Minute
 
-	if min, err := strconv.Atoi(environment.NewEnvStorage().Get("KOOL_API_TIMEOUT")); err == nil {
+	if min, err := strconv.Atoi(d.envStorage.Get("KOOL_API_TIMEOUT")); err == nil {
 		timeout = time.Duration(min) * time.Minute
 	}
 
 	var finishes chan bool = make(chan bool)
 
 	go func(deploy *api.Deploy, finishes chan bool) {
-		var lastStatus string
+		var (
+			lastStatus string
+			err        error
+		)
+
 		for {
 			err = deploy.GetStatus()
 
 			if lastStatus != deploy.Status {
 				lastStatus = deploy.Status
-				fmt.Println("  > deploy:", lastStatus)
+				d.Println("  > deploy:", lastStatus)
 			}
 
 			if err != nil {
 				finishes <- false
-				shell.Error(err)
+				d.Error(err)
 				break
 			}
 
@@ -101,24 +129,25 @@ func runDeploy(cmd *cobra.Command, args []string) {
 	case success = <-finishes:
 		{
 			if success {
-				shell.Success("Deploy finished: ", deploy.GetURL())
+				d.Success("Deploy finished: ", deploy.GetURL())
 			} else {
-				shell.Error(fmt.Errorf("deploy failed"))
-				os.Exit(1)
+				err = fmt.Errorf("deploy failed")
+				return
 			}
 			break
 		}
 
 	case <-time.After(timeout):
 		{
-			shell.Error(fmt.Errorf("timeout waiting deploy to finish"))
-			os.Exit(2)
+			err = fmt.Errorf("timeout waiting deploy to finish")
 			break
 		}
 	}
+
+	return
 }
 
-func createReleaseFile() (filename string, err error) {
+func (d *KoolDeploy) createReleaseFile() (filename string, err error) {
 	var (
 		tarball *tgz.TarGz
 		cwd     string
@@ -131,44 +160,75 @@ func createReleaseFile() (filename string, err error) {
 	}
 
 	var hasGit bool = true
-	if _, err = exec.LookPath("git"); err != nil {
+	if errGit := d.LookPath(d.git); errGit != nil {
 		hasGit = false
 	}
 
-	if _, err = os.Stat(".git"); hasGit && !os.IsNotExist(err) {
-		// we are in a Git environment
-		var (
-			output []byte
-			files  []string
-		)
-		// Exclude list
-		// git ls-files -d // delete files
-		output, err = exec.Command("git", "ls-files", "-d").CombinedOutput()
-		if err != nil {
-			panic(fmt.Errorf("Failed listing deleted "))
-		}
-		tarball.SetIgnoreList(strings.Split(string(output), "\n"))
-
-		// Include list
-		// git ls-files -c
-		output, err = exec.Command("git", "ls-files", "-c").CombinedOutput()
-		if err != nil {
-			panic(fmt.Errorf("Failed list Git cached files"))
-		}
-		files = append(files, strings.Split(string(output), "\n")...)
-		// git ls-files -o --exclude-standard
-		output, err = exec.Command("git", "ls-files", "-o", "--exclude-standard").CombinedOutput()
-		if err != nil {
-			panic(fmt.Errorf("Failed list Git untracked non-ignored files"))
-		}
-		files = append(files, strings.Split(string(output), "\n")...)
-
-		filename, err = tarball.CompressFiles(files)
-	} else {
-		fmt.Println("Fallback to tarball full current working directory...")
+	if _, errGit := os.Stat(".git"); !hasGit || os.IsNotExist(errGit) {
+		// not a GIT repo/environment!
+		d.Println("Fallback to tarball full current working directory...")
 		cwd, _ = os.Getwd()
 		filename, err = tarball.CompressFolder(cwd)
+		return
 	}
 
+	// we are in a GIT environment!
+	var (
+		output string
+		files  []string
+	)
+	// Exclude list - git ls-files -d
+	output, err = d.Exec(d.git, "ls-files", "-d")
+	if err != nil {
+		d.Error(err)
+		err = fmt.Errorf("failed listing GIT deleted files")
+		return
+	}
+	tarball.SetIgnoreList(strings.Split(output, "\n"))
+
+	// Include list - git ls-files -c
+	output, err = d.Exec(d.git, "ls-files", "-c")
+	if err != nil {
+		d.Error(err)
+		err = fmt.Errorf("failed list GIT cached files")
+		return
+	}
+	files = append(files, strings.Split(output, "\n")...)
+	// git ls-files -o --exclude-standard
+	output, err = d.Exec(d.git, "ls-files", "-o", "--exclude-standard")
+	if err != nil {
+		d.Error(err)
+		err = fmt.Errorf("failed list Git untracked non-ignored files")
+		return
+	}
+	files = append(files, strings.Split(output, "\n")...)
+
+	filename, err = tarball.CompressFiles(d.handleDeployEnv(files))
 	return
+}
+
+// handleDeployEnv tackles a special case on kool.deploy.env file.
+// This file can or cannot be versioned (good practice not to, since
+// it may include sensitive data). In the case of it being ignored
+// from GIT, we still are required to send it - it is required for
+// the Deploy API.
+func (d *KoolDeploy) handleDeployEnv(files []string) []string {
+	if _, envErr := os.Stat(koolDeployEnv); envErr == os.ErrNotExist {
+		d.Error(fmt.Errorf("missing required file: %v", koolDeployEnv))
+		return files
+	}
+
+	var isAlreadyIncluded bool = false
+	for _, file := range files {
+		if file == koolDeployEnv {
+			isAlreadyIncluded = true
+			break
+		}
+	}
+
+	if !isAlreadyIncluded {
+		files = append(files, koolDeployEnv)
+	}
+
+	return files
 }
