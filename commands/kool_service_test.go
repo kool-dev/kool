74c0571111553091556d4d18db082965bdd0de4f
fabriciojs <fajosesouza@gmail.com>
2021-06-27 00:12:38 -0300

    increase test coverage

--- commands/kool_service_test.go
+++ commands/kool_service_test.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"kool-dev/kool/core/builder"
 	"kool-dev/kool/core/shell"
+	"os"
+	"os/exec"
 	"strings"
+	"syscall"
 	"testing"
 )
 
@@ -127,5 +130,28 @@ func TestKoolServiceProxies(t *testing.T) {
 
 	if val, ok := k.shell.(*shell.FakeShell).CalledLookPath["cmd"]; !val || !ok {
 		t.Errorf("failed to assert calling method LookPath on FakeKoolService")
+	}
+}
+
+func TestKoolServiceErrors(t *testing.T) {
+	k := newFakeKoolService()
+
+	err := k.Interactive(&builder.FakeCommand{MockInteractiveError: shell.ErrLookPath}, "extraArg")
+
+	if err == nil || !strings.Contains(err.Error(), "failed to run") {
+		t.Errorf("bad error returned: %v", err)
+	}
+
+	exitError := &exec.ExitError{ProcessState: &os.ProcessState{}}
+	exitStatus := exitError.Sys().(syscall.WaitStatus).ExitStatus()
+
+	command := &builder.FakeCommand{MockInteractiveError: exitError}
+
+	err = k.Interactive(command)
+
+	if ex, ok := err.(shell.ErrExitable); !ok {
+		t.Error("should be ErrExitable")
+	} else if ex.Code != exitStatus {
+		t.Errorf("exit code should be %d but got %d", exitStatus, ex.Code)
 	}
 }
