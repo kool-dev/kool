56537fd3af5b28c846fa2debd5d54f98c7b7abca
fabriciojs <fajosesouza@gmail.com>
2021-09-13 18:46:01 -0300

    add new helper script for generating the release

---
+++ make_release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+kool run make-docs
+kool run parse-presets
+kool run fmt
+kool run lint
+kool run test
+
+if [ ! -z "$(git status -s)" ]; then
+    echo "You have uncommited changes; aborting creating release."
+    exit 1
+fi
+
+read -p "What version do you want to build (0.0.0 semver format): "
+if [[ ! $REPLY =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+then
+    echo "Bad version format; expected semver 0.0.0"
+    exit 1
+fi
+
+export BUILD_VERSION=$REPLY
+
+exec bash build_artifacts.sh
+
+# TODO: create new tag / draft a new release with github CLI
