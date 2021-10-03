600d53a0df5ff1c7305298de85885a6d5132b1dc
fabriciojs <fajosesouza@gmail.com>
2021-10-02 22:57:04 -0300

    remove commented code

--- commands/kool_service.go
+++ commands/kool_service.go
@@ -37,23 +37,3 @@ func (k *DefaultKoolService) Fake() *DefaultKoolService {
 	k.shell = &shell.FakeShell{MockIsTerminal: true}
 	return k
 }
-
-// Interactive proxies the call to the given Shell
-// func (k *DefaultKoolService) Interactive(command builder.Command, extraArgs ...string) (err error) {
-// 	err = k.shell.Interactive(command, extraArgs...)
-
-// 	if err == shell.ErrLookPath {
-// 		err = fmt.Errorf("failed to run %s error: %v", command.String(), err)
-// 		return
-// 	}
-
-// 	// Subprocess exited. Get the return code, if we can
-// 	if exitError, ok := err.(*exec.ExitError); ok {
-// 		err = shell.ErrExitable{
-// 			Err:  err,
-// 			Code: exitError.Sys().(syscall.WaitStatus).ExitStatus(),
-// 		}
-// 	}
-
-// 	return
-// }
--- commands/kool_service_test.go
+++ commands/kool_service_test.go
@@ -14,26 +14,3 @@ func TestKoolServiceProxies(t *testing.T) {
 		t.Error("unexpected Shell return")
 	}
 }
-
-// func TestKoolServiceErrors(t *testing.T) {
-// 	k := newFakeKoolService()
-
-// 	err := k.Interactive(&builder.FakeCommand{MockInteractiveError: shell.ErrLookPath}, "extraArg")
-
-// 	if err == nil || !strings.Contains(err.Error(), "failed to run") {
-// 		t.Errorf("bad error returned: %v", err)
-// 	}
-
-// 	exitError := &exec.ExitError{ProcessState: &os.ProcessState{}}
-// 	exitStatus := exitError.Sys().(syscall.WaitStatus).ExitStatus()
-
-// 	command := &builder.FakeCommand{MockInteractiveError: exitError}
-
-// 	err = k.Interactive(command)
-
-// 	if ex, ok := err.(shell.ErrExitable); !ok {
-// 		t.Error("should be ErrExitable")
-// 	} else if ex.Code != exitStatus {
-// 		t.Errorf("exit code should be %d but got %d", exitStatus, ex.Code)
-// 	}
-// }
