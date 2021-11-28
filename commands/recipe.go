b6ca05e649cdaa218c44da548d883e101670ad6a
fabriciojs <fajosesouza@gmail.com>
2021-11-28 00:42:02 -0300

    recipe usage - add listing + introducing recipe metadata

--- commands/recipe.go
+++ commands/recipe.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	_ "embed"
+	"kool-dev/kool/core/automate"
 	"kool-dev/kool/core/presets"
 
 	"github.com/spf13/cobra"
@@ -30,7 +31,31 @@ func NewKoolRecipe() *KoolRecipe {
 
 // Execute runs the add logic with incoming arguments.
 func (p *KoolRecipe) Execute(args []string) (err error) {
-	var recipe = args[0]
+	var recipe string
+
+	if len(args) == 0 {
+		// no recipe; let's just print them all
+		var metas []*automate.RecipeMetadata
+
+		if metas, err = automate.GetRecipes(); err != nil {
+			return
+		}
+
+		p.Shell().Warning("You need to provide the recipe name as argument.")
+		p.Shell().Println("")
+		p.Shell().Println("Available recipes:")
+
+		for _, meta := range metas {
+			if meta.Title != "" {
+				p.Shell().Printf("  %s (%s)\n", meta.Slug, meta.Title)
+			} else {
+				p.Shell().Printf("  %s\n", meta.Slug)
+			}
+		}
+		return
+	}
+
+	recipe = args[0]
 
 	err = presets.NewParser().Add(recipe, p.Shell())
 
@@ -43,7 +68,7 @@ func NewRecipeCommand(recipe *KoolRecipe) (recipeCmd *cobra.Command) {
 		Use:                   "recipe [RECIPE]",
 		Short:                 "Adds configuration for some recipe in the current work directory.",
 		Long:                  `Run the defines steps for a recipe which can add/edit files the current project directory in order to add some new service or configuration.`,
-		Args:                  cobra.ExactArgs(1),
+		Args:                  cobra.MaximumNArgs(1),
 		RunE:                  DefaultCommandRunFunction(recipe),
 		DisableFlagsInUseLine: true,
 	}
--- core/automate/recipes.go
+++ core/automate/recipes.go
@@ -1,9 +1,43 @@
 package automate
 
-import "embed"
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type RecipeMetadata struct {
+	Title string `yaml:"title"`
+	Slug  string
+}
 
 var recipesSource embed.FS
 
 func SetRecipesSource(src embed.FS) {
 	recipesSource = src
+}
+
+func GetRecipes() (recipes []*RecipeMetadata, err error) {
+	var (
+		entries []fs.DirEntry
+		raw     []byte
+	)
+
+	if entries, err = recipesSource.ReadDir("recipes"); err != nil {
+		return
+	}
+
+	for _, e := range entries {
+		m := &RecipeMetadata{Slug: strings.ReplaceAll(e.Name(), ".yml", "")}
+		raw, err = recipesSource.ReadFile(fmt.Sprintf("recipes/%s", e.Name()))
+		if err = yaml.Unmarshal(raw, m); err != nil {
+			recipes = nil
+			return
+		}
+		recipes = append(recipes, m)
+	}
+	return
 }
