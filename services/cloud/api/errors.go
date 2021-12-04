4b91ccc970d805ee95791e53ec2ba3cd8eb4a219
fabriciojs <fajosesouza@gmail.com>
2021-12-03 23:52:39 -0300

    deploy error handling improvement

--- services/cloud/api/errors.go
+++ services/cloud/api/errors.go
@@ -3,6 +3,7 @@ package api
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 // ErrBadAPIServer represents some issue in the API side
@@ -17,7 +18,7 @@ var ErrDeployFailed error
 // ErrUnauthorized unauthorized; please check your KOOL_API_TOKEN
 var ErrUnauthorized error
 
-// ErrPayloadValidation something went wrong validating the payload
+// failed validating deploy payload
 var ErrPayloadValidation error
 
 // ErrBadResponseStatus unexpected return status
@@ -28,25 +29,29 @@ var ErrUnexpectedResponse error
 
 // ErrAPI reprents a default error returned from the API
 type ErrAPI struct {
-	Status  int
-	Message string `json:"message"`
+	Status int
 
-	Errors map[string]interface{} `json:"errors"`
+	Message string                 `json:"message"`
+	Errors  map[string]interface{} `json:"errors"`
 }
 
 // Error returns the string representation for the error
 func (e *ErrAPI) Error() string {
 	if e.Errors != nil {
-		return fmt.Sprintf("%d - %s (%v)", e.Status, e.Message, e.Errors)
+		s := []string{}
+		for k, e := range e.Errors {
+			s = append(s, fmt.Sprintf("\t%s > %v", k, e.([]interface{})[0]))
+		}
+		return fmt.Sprintf("\n%d - %s\n\n%s\n", e.Status, e.Message, strings.Join(s, "\n"))
 	}
-	return fmt.Sprintf("%d - %s", e.Status, e.Message)
+	return fmt.Sprintf("\n%d - %s\n", e.Status, e.Message)
 }
 
 func init() {
 	ErrBadAPIServer = errors.New("bad API server response")
 	ErrDeployFailed = errors.New("deploy process has failed")
 	ErrUnauthorized = errors.New("unauthorized; please check your KOOL_API_TOKEN")
-	ErrPayloadValidation = errors.New("something went wrong validating the payload")
+	ErrPayloadValidation = errors.New("failed validating deploy payload")
 	ErrBadResponseStatus = errors.New("unexpected return status")
 	ErrUnexpectedResponse = errors.New("bad API response; please ask for support")
 	ErrMissingToken = errors.New("missing KOOL_API_TOKEN")
