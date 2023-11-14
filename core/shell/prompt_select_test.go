7bf29cd7d2bf316b750f3c62b73a32b695e2ddad
fabriciojs <fajosesouza@gmail.com>
2023-11-14 19:42:02 -0300

    test actual Confirm prompt question

--- core/shell/prompt_select_test.go
+++ core/shell/prompt_select_test.go
@@ -17,9 +17,7 @@ func TestNewPromptSelect(t *testing.T) {
 
 func TestAskPromptSelect(t *testing.T) {
 	oldStdout := os.Stdout
-
 	r, w, _ := os.Pipe()
-
 	os.Stdout = w
 
 	p := NewPromptSelect()
@@ -38,5 +36,29 @@ func TestAskPromptSelect(t *testing.T) {
 
 	if !strings.Contains(output, "testing_question") || !strings.Contains(output, "testing_option1") || !strings.Contains(output, "testing_option2") {
 		t.Error("failed to render the question and its options")
+	}
+}
+
+func TestConfirmPromptSelect(t *testing.T) {
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	p := NewPromptSelect()
+
+	_, _ = p.Confirm("testing_question %s", "arg1")
+
+	w.Close()
+	out, err := io.ReadAll(r)
+	os.Stdout = oldStdout
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	output := string(out)
+
+	if !strings.Contains(output, "testing_question arg1") || !strings.Contains(output, "Yes") || !strings.Contains(output, "No") {
+		t.Error("failed to render the Confirm prompt and its options")
 	}
 }
